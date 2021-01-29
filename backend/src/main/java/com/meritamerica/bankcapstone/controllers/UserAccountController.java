00316fe041766a34f6c496c3dfec79653b04ac36
Rod <rodwombles@gmail.com>
2021-01-29 04:19:03 -0600

    On branch Backend-WIP Changes to be committed: 	modified:   src/main/java/com/meritamerica/bankcapstone/controllers/UserAccountController.java 	modified:   src/main/java/com/meritamerica/bankcapstone/controllers/UserAccountController.java

--- backend/src/main/java/com/meritamerica/bankcapstone/controllers/UserAccountController.java
+++ backend/src/main/java/com/meritamerica/bankcapstone/controllers/UserAccountController.java
@@ -1,16 +1,21 @@
 package com.meritamerica.bankcapstone.controllers;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.meritamerica.bankcapstone.models.SavingsAccount;
 import com.meritamerica.bankcapstone.models.User;
 import com.meritamerica.bankcapstone.services.UserAccountService;
 
@@ -20,19 +25,75 @@ public class UserAccountController {
 	@Autowired
 	UserAccountService service;
 	
-	// Get user:
+	// User APIs =======================================================
+	
+	// Get all users:
 	
 	@GetMapping(value = "/Users")
+	@ResponseStatus(HttpStatus.OK)
 	public List<User> getUsers(){
 		return service.getUsers();
 	}
 	
+	// Get user by id:
+	
+	@GetMapping("/Users/{id}/")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<User> fetchUserById(@PathVariable("id") Long id) {
+		return service.getUserById(id);
+	}
+	
 	// Post user:
 	
-	@PostMapping(value = "/Users")
+	@PostMapping(value = "/Users", consumes = "application/json", produces = "application/json")
 	@ResponseStatus(HttpStatus.CREATED)
-	public User addUser(User user) {
+	public String addUser(User user) {
 		service.addUser(user);
-		return user;
+		return user.toString();
 	}
+	
+	// Delete user:
+	
+	@DeleteMapping("/Users/{id}/")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<User> removeUserById(@PathVariable("id") Long id) {
+		service.removeUserById(id);
+		return null;
+	}
+	
+	// Savings Account APIs =======================================================
+
+	// Get all saving accounts:
+	
+		@GetMapping(value = "/Users/SavingsAccount")
+		@ResponseStatus(HttpStatus.OK)
+		public List<SavingsAccount> getSavings(){
+			return service.getSavingsAccounts();
+		}
+		
+		// Get savings account by id:
+		
+		@GetMapping("/Users/SavingsAccount/{id}/")
+		@ResponseStatus(HttpStatus.OK)
+		public Optional<SavingsAccount> fetchSavingsById(@PathVariable("id") Long id) {
+			return service.getSavingsAccountById(id);
+		}
+		
+		// Post savings account:
+		
+		@PostMapping(value = "/Users", consumes = "application/json", produces = "application/json")
+		@ResponseStatus(HttpStatus.CREATED)
+		public String addUser(User user) {
+			service.addUser(user);
+			return user.toString();
+		}
+		
+		// Delete user:
+		
+		@DeleteMapping("/Users/{id}/")
+		@ResponseStatus(HttpStatus.OK)
+		public Optional<User> removeUserById(@PathVariable("id") Long id) {
+			service.removeUserById(id);
+			return null;
+		}
 }
