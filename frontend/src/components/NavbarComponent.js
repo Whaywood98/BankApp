07bdfbde613675133279ffb79ff4a8a85be0e4a3
William Haywood <william.haywood981@gmail.com>
2021-02-13 12:21:54 -0500

    added team photos to about us

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -53,6 +53,11 @@ function Navbar() {
                         </Link>
                         </li> 
                         <li className='nav-item'>
+                            <Link to= '/aboutus' className= 'nav-links' onClick={closeMobileMenu}>
+                                About Us
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
                             <Link to= '/dashboard' className= 'nav-links' onClick={closeMobileMenu}>
                                 Dashboard
                             </Link>
